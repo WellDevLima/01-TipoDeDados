9fa1cc40d1c225ebcf4f5436bafce3938c566fb3
Wellington Ferreira <rekak46923@segichen.com>
2024-08-13 12:20:11 -0300

    Edit do well

--- 1_TipoDados/TiposDeDados/TiposDeDados/TiposDeDados.cpp
+++ 1_TipoDados/TiposDeDados/TiposDeDados/TiposDeDados.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main()
 {
 	cout << "Estrutura de Dados - Fatec" << endl;
-	cout << "Do Wellington"
+	cout << "Do Wellington" << endl;
 	
 	cout << "Tipos Numericos\n";
 	cout << "===============\n";
